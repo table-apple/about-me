5e3ef9e7c2c9e05eed9ccd48e83099375016fa2d
Ashutosh Pattanaik <ashutosh.pattanaik28@gmail.com>
2019-12-15 17:46:23 +0530

    Changelog - 4. Added when a user presses done button, the nickname field and done button are hidden and are the entered nickname is displayed on the screen 3. Added a Done button 2. Added nickname input field 1. Added ScrollView - displays a text view (long text)

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val doneButton: Button = findViewById(R.id.done_button)
+        doneButton.setOnClickListener {
+            displayNickname(it)
+        }
+    }
+
+    private fun displayNickname(view: View){
+        val nicknameField: TextView = findViewById(R.id.nickname_field)
+        val nicknameDisplay = findViewById<TextView>(R.id.nickname_display)
+        nicknameDisplay.text = nicknameField.text
+        nicknameField.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameDisplay.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
